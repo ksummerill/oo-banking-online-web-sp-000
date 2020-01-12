1663c34da7789324a9a55c26974e04360730faeb
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-12 19:07:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @sender.valid?
+      @sender.valid? == false
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
