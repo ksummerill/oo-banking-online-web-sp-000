49b54b1c76878e7e2c1dd61828ecbd1cb8f4e1e1
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-12 19:24:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,15 +18,30 @@ class Transfer
   # can execute a successful transaction between two accounts
   # each transfer can only happen once
   # rejects a transfer if the sender does not have enough funds (does not have a valid account)
+  # def execute_transaction
+  #   if @sender.balance < @amount && @sender.close_account == "closed"
+  #     @status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   elsif @status == "complete"
+  #     puts "Transaction has already been executed"
+  #   else @status = "pending"
+  #     @sender.deposit(-@amount)
+  #     @receiver.deposit(@amount)
+  #     @status = "complete"
+  #   end
+  # end
+
+
   def execute_transaction
-    if @sender.balance < @amount && @sender.close_account == "closed"
+    if @sender.balance < @amount
       @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
+
     elsif @status == "complete"
-      puts "Transaction has already been executed"
-    else @status = "pending"
-      @sender.deposit(-@amount)
-      @receiver.deposit(@amount)
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
       @status = "complete"
     end
   end
